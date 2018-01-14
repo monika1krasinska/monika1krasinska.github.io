6ee647cb456a0dbe28f00fb17c97be064cd1d994
Monika Krasińska <monika.krasinska@o2.pl>
2018-01-14 10:54:58 +0100

    commit -m "document-ready"

---
+++ warsztaty/kalkulator-odsetek/js/main.js
@@ -0,0 +1,15 @@
+'use strict';
+var wszystkieZgody = document.getElementById("wszystkie=zgody");
+var checkboxy = document.querySelectorAll('input[type=checkbox]');
+//console.log(checkboxy);
+//console.log(wszystkieZgody);
+var dwaCheckboxy = document.getElementsByClassName(.checkbox -required)
+function checkboxState (){
+    if (wszystkieZgody.checked) {
+        return true;
+    }
+else {
+    return false;
+}
+}
+    console.log(dwaCheckboxy);
