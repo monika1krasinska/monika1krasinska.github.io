27ef71bf1c5088f67d190ab6511a5818e0d671b6
Monika Krasińska <monika.krasinska@o2.pl>
2018-01-24 19:46:48 +0100

    commit -m "poprawki"

--- ajax/zadanie-domowe/js/main.js
+++ ajax/zadanie-domowe/js/main.js
@@ -1,17 +1,16 @@
 'use strict';
 $(function () {
+    $("body").append("<div>");
+    $("div").attr("id", "dane-programisty");
+
     function pobierzDane(method, url) {
         console.log('dziala');
         $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php",
             function (data) {
                 console.log(data);
-                $("body").append("<div>");
-                $("div").attr("id", "dane-programisty");
                 $("#dane-programisty").append("<p>" + data.imie + " " + data.nazwisko + " " + data.zawod + " " + data.firma + "</p>")
             })
     }
     var przycisk = document.getElementById("przycisk");
     przycisk.onclick = pobierzDane;
 })
-
-
