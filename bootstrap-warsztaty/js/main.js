18a14010d4fcee67938d48f66409df497c604ffe
Monika Krasińska <monika.krasinska@o2.pl>
2018-01-28 16:26:01 +0100

    commit -m"scroll"

--- bootstrap-warsztaty/js/main.js
+++ bootstrap-warsztaty/js/main.js
@@ -1,1 +1,15 @@
 'use strict';
+$(function (){
+  $(window).scroll(function(){
+    var win = $(window);
+    if(win.scrollTop()>= 50){
+        $("#main-nav").addClass("scroll");
+    } else {
+        $("#main-nav").removeClass("scroll");}
+});
+
+  $("a.[href^='#']").on("click",function(){
+      var elementTarget = $(this.hash);
+      $("html,body").animate({scrollTop:elementTarget.offset().top -74},600)
+  })
+  })
